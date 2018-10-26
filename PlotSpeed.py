999c031790ee9699c338fa61304d4a10f91cb078
irshadmuaz <muaza@clemson.edu>
2018-10-26 09:17:59 -0400

    stable processing script

---
+++ PlotSpeed.py
@@ -0,0 +1,57 @@
+import sys
+import re
+import math
+import matplotlib.pyplot as plt
+import collections
+if len(sys.argv) < 3:
+    print('USAGE: <Real> <spoofed>')
+    exit(-1)
+def extract(filename):
+    lines = open(filename,'r').readlines()
+    velocity = []
+    times = []
+    for line in lines[1:]:
+        #match = re.search(r'\s+(-?\d+\.?\d+?)\s+(-?\d+\.?\d+?)\s+(-?\d+\.?\d+?)\s+(-?\d+\.?\d+?)\s+(\d+)',line,re.I|re.M)
+        match = re.search(r'\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)',line,re.I|re.M)
+        if match:
+            data = match.groups()
+            sum = 0
+            for v in data[:-2]:
+                sum+=(float(v))**2
+            vel = math.sqrt(sum)
+            time = int(data[-1])
+            velocity.append(vel*3.6)
+            times.append(time)
+    print('len of ' + filename + ' ' + str(len(velocity)))
+    return velocity,times
+def velocities():
+    vel,t = extract(sys.argv[1])
+    vel2,t2 = extract(sys.argv[2])
+    print('extracted_speeds', len(vel),len(vel2))
+
+    Dt2 = {x:index for index,x in enumerate(t2)}
+    spoofed_velocity = []
+    actual_velocity = []
+    t_actual = []
+    diff= t[0] - t2[0]
+    trim_index = 0
+    while vel2[trim_index] == 0:
+        trim_index+=1
+    t2 = t2[trim_index:]
+    t = t[trim_index:]
+    actual_velocity = vel[trim_index:]
+    spoofed_velocity = vel2[trim_index:]
+    t_actual = t2
+    #print(len(spoofed_velocity),len(actual_velocity))
+    print('velocities',len(vel),len(vel2),t[-1] - t[0] ,t[1] - t2[1], t[0] - t2[0])
+
+    plt.plot(t_actual,actual_velocity,'r-',t_actual,spoofed_velocity,'b-')
+    difference = [(spoofed_velocity[i] - actual_velocity[i]) for i in range(len(actual_velocity))]
+    # print(difference)
+    # plt.plot(t_actual,difference,'b-')
+    # plt.xlabel('Time(sec)')
+    # plt.ylabel('Velocity (kmph)')
+    #plt.legend(['actual speed','spoofer_speed'])
+    plt.legend(['speed difference'])
+    plt.show()
+    return t_actual,difference
---
+++ process.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 23 18:31:05 2018
+
+@author: mahmad
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import PlotSpeed as speed
+class velocity:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.z = 0
+    def mag(self):
+        return math.sqrt(self.x**2 + self.y**2 +self.z**2)
+class doppler:
+    def __init__(self):
+        self.prn = 0
+        self.time = 0
+        self.pos = 0
+        self.velocity = 0
+        self.measured = 0
+        self.calculated = 0
+file = open('range_data.log_doppler')
+vel = velocity()
+pos = velocity()
+dop = doppler()
+lines = file.readlines()
+dopplers = {i:[] for i in range(40)}
+for line in lines[1:]:
+    pieces = line.split(' ')
+    pieces = [x for x in pieces if x != '']
+# =============================================================================
+#     if pieces[0] == 'Velocity':
+#         vel = velocity()
+#         vel.x = float(pieces[2])
+#         vel.y = float(pieces[3])
+#         vel.z = float(pieces[4])
+#
+#     elif pieces[0] == 'Position':
+#         pos.x = float(pieces[2])
+#         pos.y = float(pieces[3])
+#         pos.z = float(pieces[4])
+#     elif pieces[0] == 'Doppler':
+# =============================================================================
+    dop = doppler()
+    dop.time = float(pieces[10])
+    dop.measured = float(pieces[2])
+    dop.calculated = float(pieces[3])
+    dop.prn = int(pieces[0])
+    dopplers[int(pieces[0])].append(dop)
+
+_time,difference = speed.velocities()
+print("Showing  speed difference")
+plt.plot(_time,difference,'b-')
+plt.show()
+for i in dopplers:
+    x = []
+    y = []
+    z = []
+    average = 0
+    average2 = 0
+    count,pop,_count = 0,0,2
+    if dopplers[i] != []:
+        dops = dopplers[i]
+        for dop in dops:
+            count+=1
+            average += dop.measured
+            average2 += dop.calculated
+            if count== _count:
+                x.append(dop.time)
+                y.append(average/count)
+                #print(dop.velocity.mag())
+                z.append(average2/count)
+                average -= dops[pop].measured
+                average2-= dops[pop].calculated
+                count-=1
+                pop+=1
+        dop_diff = [(y[j] - z[j]) for j in range(len(y))]
+        plt.subplot(211)
+        plt.title('SAT_' + str(i))
+        #print(i, x[0],_time[0],len(x),len(_time))
+
+        d = len(_time) - len(x)
+        tm = []
+        df = []
+        if d > 0:
+            for a,b in enumerate(_time):
+                if(b>=x[0] and b <= x[-1]):
+                    tm.append(b)
+                    df.append(difference[a])
+        else:
+            continue;
+        #tdiff = tm[0] - x[0]
+        #tm = [z - tdiff for z in tm]
+        print(i, x[0],tm[0],len(x),len(tm))
+        plt.plot(x,dop_diff,'r-')
+        plt.xlabel('Time (sec)')
+        plt.ylabel('Doppler (Hz)')
+        plt.subplot(212)
+        plt.plot(tm,df,'b-')
+        plt.xlabel('Time (sec)')
+        plt.ylabel('Speed Km/h')
+        plt.savefig('satellite_' + str(i) +'.png')
+        plt.close()
+        plt.plot(x,y,'r',x,z,'b')
+        plt.legend(['measured','predicted'])
+        plt.savefig('satellite_' + str(i) +'_abs_' + '.png')
